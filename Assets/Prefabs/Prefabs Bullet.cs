a595b8e6531da4d74ddf83f97a8327b14b70f07f
jopatty <chickimiau701@gmail.com>
2025-02-27 16:46:59 +0700

    senjata nembak

--- Assets/Prefabs/Prefabs Bullet.cs
+++ Assets/Prefabs/Prefabs Bullet.cs
@@ -5,29 +5,25 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     //bullet travel spd
-    [Range(1.10)]
+    [Range(1,10)]
     [SerializeField] private float speed = 10f;
 
     //destroy after second
-    [Range(1.10)]
+    [Range(1,10)]
     [SerializeField] private float lifeTime = 4f;
 
     private Rigidbody2D rb;
 
-    // Start is called before the  first frame update
+    // Start is called before the first frame update
     private void Start ()
     {
         rb = GetComponent<Rigidbody2D>();
         Destroy(gameObject, lifeTime);
     }
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    //Update is called once per frame
+    private void FixedUpdate()
     {
-        
+        rb.linearVelocity = transform.right * speed;
     }
 }
